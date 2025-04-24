452bbed792354d0da33cadedaf21bffdc5106605
Gamesrol <gs.gore@gmail.com>
2025-04-24 10:24:06 +0200

    Holaaa

--- dia3/e19.js
+++ dia3/e19.js
@@ -6,6 +6,9 @@ var options = {
   height: 100
 }
 
+// Asciify the image
+// The image will be resized to fit in a box of 200x100 characters  
+
 asciify('img/1.png', options, function (err, asciified) {
   if (err) throw err;
 
